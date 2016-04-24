2b33a331df0c2a28bc2903aa3d2d9dad3f69b22f
MikaAK <me@mikakalathil.ca>
2016-04-24 02:41:02 -0700

    Change all keys to snake_case

--- lib/trello_http.ex
+++ lib/trello_http.ex
@@ -8,21 +8,34 @@ defmodule Trello.Http do
   end
 
   defp decode_body(body) do
-    with {:ok, data} <- Poison.decode(body), do: atomize_keys(data)
+    with {:ok, data} <- Poison.decode(body), do: snake_keys(data) |> atomize_keys
+  end
+
+  defp snake_keys(data) when is_map(data) do
+    for {key, val} <- data, into: %{} do
+      key_name = Macro.underscore key
+
+      {key_name, snake_keys(val)}
+    end 
   end
 
   defp atomize_keys(data) when is_map(data) do
     for {key, val} <- data, into: %{} do
-      key_name = String.to_atom(key)
+      key_name = String.to_atom key
 
       {key_name, atomize_keys(val)}
     end
   end
   
+  defp snake_keys(data) when is_list(data) do
+    for item <- data, into: [], do: snake_keys(item)
+  end
+
   defp atomize_keys(data) when is_list(data) do
     for item <- data, into: [], do: atomize_keys(item)
   end
 
   defp atomize_keys(data), do: data
+  defp snake_keys(data), do: data
   defp is_json(string), do: Regex.match?(~r/^({|\[).*(}|\])$/, string)
 end
